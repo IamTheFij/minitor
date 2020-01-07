5a7adbcff5148c2f1429a61e8904633dbded8ea8
Ian Fijolek <ian@iamthefij.com>
2020-01-06 19:15:34 -0800

    Drop Python 3.4 support

--- setup.py
+++ setup.py
@@ -29,7 +29,6 @@ setup(
         'Topic :: System :: Monitoring',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
